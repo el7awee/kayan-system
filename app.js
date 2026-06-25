fd888a789210229192935b7a30c108bfa36dda2e
el7awee <el7awee@gmail.com>
2026-06-25 15:23:26 +0300

    تحديث BACKEND_API_URL إلى v46 + نشر soft-delete

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
  */
 
 // ─── 1️⃣ الإعدادات والثوابت العالمية ───
-const BACKEND_API_URL = "https://script.google.com/macros/s/AKfycbx0tfLisbc162MX8W8zIyoQO-Um5l-Z-1beFJGeJuU9ohKW8okJaRlbmZBfIta6Bxqs/exec";
+const BACKEND_API_URL = "https://script.google.com/macros/s/AKfycbwTaLdsu6hAfZtJdlEavVdKBVniXo4VV7_W3AwkV9gljjiuRM9N1RplLWc_WB3OstrQ/exec";
 
 // حالة التطبيق المحلية
 const state = {
